f9c1baad5d86a0baaaba2ae1d7723c4f613fb36b
fumito-2310 <fumito2310_0111@yahoo.co.jp>
2019-10-25 19:59:41 +0900

    mypage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   root "users#show"
+
+  resources :user
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

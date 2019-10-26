2fc84ea064ebccd8f3ef0bd2e6a5468d1f6e2d84
fumito-2310 <fumito2310_0111@yahoo.co.jp>
2019-10-26 22:44:20 +0900

    mypage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,16 @@
 class UsersController < ApplicationController
  def show
  end
+
+ def new
+    @user = current_user.id
+    @user.create(user_params)
+   else
+ end
+
+ private
+
+ def user_params
+ params.permit(:address_number,:address_prefecture,:address_name,:address_block,:address_building)
+ end
 end

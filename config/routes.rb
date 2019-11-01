6345fedb67df449f9cc5fec40611ed2d46a53421
STHEXA <ti.syu.s@gmail.com>
2019-11-01 16:18:48 +0900

    Changed front form to helper method specification.
    The credit card registration screen is pending because form_for had an error.

--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
+  # devise_for :users
+  # root 'items#index'
+  # resources :users
+  # resources :items
+
+  root to: 'signup#step4'  
   devise_for :users
-  root 'items#index'
-  resources :users
-  resources :items
+  root  'users#index'
 
-  # root to: 'signup#step1'  # devise_for :users
-  # # root  'users#index'
-
-  # resources :signup do
-  #   collection do
-  #     get 'step1'
-  #     get 'step2'
-  #     get 'step3'
-  #     get 'step4' # ここで、入力の全てが終了する
-  #     post 'create'
-  #     get 'done' # 登録完了後のページ
-  #   end
-  # end
+  resources :signup do
+    collection do
+      get 'step1'
+      get 'step2'
+      get 'step3'
+      get 'step4' # ここで、入力の全てが終了する
+      post 'create'
+      get 'done' # 登録完了後のページ
+    end
+  end
 end

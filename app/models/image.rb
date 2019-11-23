e43fe5a591d743ef2684f309eee37c5c5834e8fb
Jim-1227 <hajime.minamimoto@gmail.com>
2019-11-23 15:27:06 +0900

    create_images_table

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,3 @@
 class Image < ApplicationRecord
-  belongs_to :item, optional: true
-  mount_uploader :file_name, ImageUploader
+  belongs_to :item
 end
--- db/migrate/20191123062417_create_images.rb
+++ db/migrate/20191123062417_create_images.rb
@@ -1,8 +1,8 @@
 class CreateImages < ActiveRecord::Migration[5.0]
   def change
     create_table :images do |t|
-      t.string :file_name
-
+      t.string :file_name, null: false
+      t.references :item, foreign_key: true, null: false
       t.timestamps
     end
   end

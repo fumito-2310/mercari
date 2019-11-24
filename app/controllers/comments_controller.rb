44ca0146ff8c79a9307897cd7a2710093f0f8cdd
Jim-1227 <hajime.minamimoto@gmail.com>
2019-11-24 12:37:48 +0900

    test-commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     # user_idは仮置き。本来はcurrent_user.id
     comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: comment_params[:user_id])
+    binding.pry
     redirect_to "/items/#{comment.item.id}"
   end
 

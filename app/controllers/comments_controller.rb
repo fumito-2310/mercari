ba721bd1728afb5899a3f9c940d2d9fe794ab4ed
STHEXA <ti.syu.s@gmail.com>
2019-11-25 15:40:10 +0900

    Change user ID to temporary

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,12 @@
 class CommentsController < ApplicationController
   def create
     # user_idは仮置き。本来はcurrent_user.id
-    comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: comment_params[:user_id])
+    comment = Comment.create(text: comment_params[:text], item_id: comment_params[:item_id], user_id: 1)
     redirect_to "/items/#{comment.item.id}"
   end
 
   private
   def comment_params
-    params.permit(:text, :item_id, :user_id)
+    params.permit(:text, :item_id)
   end
 end

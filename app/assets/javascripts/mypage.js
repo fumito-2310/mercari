6a24004f083c888b80f8a107692990d2ee5124e3
STHEXA <ti.syu.s@gmail.com>
2019-11-30 12:39:28 +0900

    Improved to ignite JS without correcting the view collapse and updating

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,9 +1,9 @@
 $(function(){
 
-  $(".product-list").hide();
+  $(".product-list__contents").hide();
   $(".list__item.exhibiting").on("click", function(e){
     $(".main").remove();
-    $('.product-list').show();
+    $('.product-list__contents').show();
     console.log('kita');
     e.preventDefault();
   });

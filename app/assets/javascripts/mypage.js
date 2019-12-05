ed99c39a404540a68ed81d7a2af362a2e9ac42d5
STHEXA <ti.syu.s@gmail.com>
2019-12-05 15:36:17 +0900

    Changed to fire events using turbolinks

--- app/assets/javascripts/mypage.js
+++ app/assets/javascripts/mypage.js
@@ -1,10 +1,12 @@
-$(function(){
+$(window).on("turbolinks:load", function() {
+  $(function(){
 
-  $(".product-list__contents").hide();
-  $(".list__item.exhibiting").on("click", function(e){
-    $(".main").remove();
-    $('.product-list__contents').show();
-    e.preventDefault();
-  });
+    $(".product-list__contents").hide();
+    $(".list__item.exhibiting").on("click", function(e){
+      $(".main").remove();
+      $('.product-list__contents').show();
+      e.preventDefault();
+    });
 
+  })
 })
